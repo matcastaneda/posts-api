afc222666cec30431fbb3e25de5380ad953914de
Matías Castañeda Mondaca <matiascmondaca@gmail.com>
2023-03-11 18:58:49 -0300

    📝 Add credentials for export

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,13 @@ import { config } from 'dotenv';
 
 config();
 
-export const { PORT, CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, MONGO_URI } = process.env;
+export const {
+  PORT,
+  NODE_ENV,
+  CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET,
+  MONGO_URI,
+  JWT_SECRET,
+  JWT_EXPIRES_IN,
+} = process.env;
